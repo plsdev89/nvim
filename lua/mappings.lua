8333cbdb9a9989fe9924359d08930ba8a5f78deb
plsdev89 <plsdev89@gmail.com>
2024-11-21 09:30:11 -0500

    Enable arrow keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,18 +10,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map("n", "<Up>", "<Nop>", { noremap = true, silent = true })
-map("n", "<Down>", "<Nop>", { noremap = true, silent = true })
-map("n", "<Left>", "<Nop>", { noremap = true, silent = true })
-map("n", "<Right>", "<Nop>", { noremap = true, silent = true })
-map("i", "<Up>", "<Nop>", { noremap = true, silent = true })
-map("i", "<Down>", "<Nop>", { noremap = true, silent = true })
-map("i", "<Left>", "<Nop>", { noremap = true, silent = true })
-map("i", "<Right>", "<Nop>", { noremap = true, silent = true })
-map("v", "<Up>", "<Nop>", { noremap = true, silent = true })
-map("v", "<Down>", "<Nop>", { noremap = true, silent = true })
-map("v", "<Left>", "<Nop>", { noremap = true, silent = true })
-map("v", "<Right>", "<Nop>", { noremap = true, silent = true })
 map(
   "n",
   "<leader>bc",
